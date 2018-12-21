4f2c0460516b5fdf42707423e58c97aa86d0153e
Sean Black <mailseanblack@yahoo.com>
2018-12-21 16:12:31 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const hbs = require("hbs");
+const fs = require("fs");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+hbs.registerPartials(path.join(__dirname, "views", "partials"));
+
+app.set("view engine", "hbs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile("server.log", log + "\n", err => {
+    if (err) {
+      console.log("unable to append to server .log");
+    }
+  });
+  next();
+});
+
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
+
+app.get("/", (req, res, next) => {
+  res.render("home.hbs", {
+    pageTitle: "Home Page"
+  });
+});
+
+app.get("/about", (req, res, next) => {
+  res.render("about.hbs", {
+    pageTitle: "About Page"
+  });
+});
+
+app.get("/bad", (req, res, next) => {
+  res.send({
+    name: "error",
+    problem: ["Unfortunately the page you are looking for does not exist"]
+  });
+});
+
+app.use("/maintanence", (req, res, next) => {
+  res.render("maintanence.hbs");
+});
+app.listen(3000, () => {
+  "The Server is Running on Port 3000";
+});
